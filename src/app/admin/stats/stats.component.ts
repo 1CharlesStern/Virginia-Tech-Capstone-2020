7c50da53029e8acf57cab50597d9b562a79e6f5e
Woody McIlhenny <WoodyMcIlhenny@Allisons-MacBook-Pro-4.local>
2020-04-24 15:19:21 -0400

    admin stats display interviews

--- src/app/admin/stats/stats.component.ts
+++ src/app/admin/stats/stats.component.ts
@@ -1,4 +1,31 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  //
+import {Observable} from "rxjs/Observable";         //
+import * as _ from 'lodash';
+
+interface Company { //
+  id: number,       //
+  name: string,     //
+  url: string       //
+}                   //
+
+interface CareerFair {        //
+  id: number,                 //
+  name: string,               //
+  numberOfCompanies: number,  //
+  numberOfStudents: number,   //
+  numberOfInterviews: number  //
+}                             //
+
+interface Interview {       //
+  id: string,               //
+  studentIDNumber: number,  //
+  studentName: string,      //
+  companyID: number,        //
+  date: string,             //
+  time: string,             //
+  careerFairID: number      //
+}                           //
 
 @Component({
   selector: 'app-stats',
@@ -15,6 +42,8 @@ export class StatsComponent implements OnInit {
 
   curPage = 0
 
+  cfid = 0;
+
   PAGE_SIZE = 10
 
   //Should match width of "limited" class in stylesheet
@@ -27,27 +56,74 @@ export class StatsComponent implements OnInit {
   @ViewChild("timeDown") timeDown: ElementRef;
   @ViewChild("timeUp") timeUp: ElementRef;
 
-  data = [
-    {name: "Bob Jones", company: "General Electric", time: new Date("March 30 2020 1:41 PM")},
-    {name: "Mary Phillips", company: "General Electric", time: new Date("March 30 2020 1:35 PM")},
-    {name: "Joe Rogan", company: "General Electric", time: new Date("March 30 2020 1:00 PM")},
-    {name: "Jeff Bezos", company: "General Electric", time: new Date("March 30 2020 11:55 AM")},
-    {name: "Stevie Wonder", company: "General Electric", time: new Date("March 29 2020 2:00 PM")}
-  ]
+  data = [];
 
-  selectedFair = "Fall 2020"
+  selectedFair = "";
 
-  careerFairs = [
-    "Fall 2020",
-    "Spring 2020",
-    "Fall 2019",
-    "Spring 2019"
-  ]
+  careerFairs = [];
 
-  constructor() { }
+  compObjs = [];
+
+  constructor(private http: HttpClient) { }
+
+  getCompanies(): void {
+    this.http.get<Company[]>("http://localhost:8080/api/companies.json") // testing
+      .map(data => _.values(data))                                            //
+      .subscribe(data => {                                                    //
+
+        this.compObjs = data;
+    });;
+  }
+
+  getCareerFairs(): void {
+    this.http.get<CareerFair[]>("http://localhost:8080/api/careerfairs.json") // testing
+      .map(data => _.values(data))                                            //
+      .subscribe(data => {                                                    //
+
+        this.cfid = Math.max.apply(Math, data.map(a => a.id));                                         //
+        this.selectedFair = data.find(o => o.id === this.cfid).name;
+        this.careerFairs = data.map(a => a.name);
+    });;
+  }
+
+  getInterviews(): void {
+    this.http.get<Interview[]>("http://localhost:8080/api/interviews.json") // testing
+      .map(data => _.values(data))                                            //
+      .subscribe(data => {
+
+        this.numStudents = 0;
+        this.numCompanies = 0;                                             //
+
+        for (let item of data) {
+          console.log(item);
+          console.log(this === undefined);
+          if(item.careerFairID === this.cfid) {
+
+            let companyName = this.compObjs.find(o => o.id === item.companyID).name;
+
+            // increment numStudents/numCompanies if unique
+            if (this.data.find(o => o.name === item.studentName) === undefined) { this.numStudents++ }
+            if (this.data.find(o => o.company === companyName) === undefined) { this.numCompanies++ }
+
+            this.data.push({
+              name: item.studentName,
+              company: companyName,
+              time: new Date(item.date.toString() + 'T' + item.time.toString())
+            });
+          }
+        }
+
+        this.numInterviews = this.data.length                                   //
+    });;
+  }
 
   ngOnInit(): void {
+    this.getCompanies();
+    this.getCareerFairs();
+    this.getInterviews();
   }
+
+
 
   sort(index: Number, isAscending: Boolean){
     //TODO sorting
