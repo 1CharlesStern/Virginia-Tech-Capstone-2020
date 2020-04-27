257c301f1638d721c3f4f349c7226edeccd98e64
Charles <1charlesstern@gmail.com>
2020-04-27 10:28:34 -0400

    Finalized the company master list

--- src/app/admin/masterlist/masterlist.component.ts
+++ src/app/admin/masterlist/masterlist.component.ts
@@ -9,7 +9,7 @@ import { AdminService } from '../admin.service';
 @Injectable()
 export class MasterlistComponent implements OnInit {
 
-  ascendingSort = false
+  ascendingSort = true
 
   constructor(private service: AdminService) { }
 
@@ -22,20 +22,25 @@ export class MasterlistComponent implements OnInit {
   curPage = 0;
 
   data = [
-    {name: "General Electric", url: ""},
-    {name: "Toshiba", url: ""},
-    {name: "Cisco", url: ""},
-    {name: "Google", url: ""},
-    {name: "Facebook", url: ""},
-    {name: "Amazon", url: ""},
+    /*
+    example: {
+    "id" : 4,
+    "name" : "company",
+    "url" : "http://epsilon.cs.vt.edu/company/y42fp",
+    "interviews" : []
+  }*/
   ]
 
   paginate(page: number): void{
     if (page < 0){
       page = 0
     }
-    if (page > (this.data.length/9)-1){
-      page = (this.data.length/9)-1
+    let max_page = Math.floor(this.data.length/this.PAGE_SIZE)
+    if (max_page % this.PAGE_SIZE == 0){
+      max_page--
+    }
+    if (page > max_page){
+      page = max_page
     }
     this.curPage = page
   }
@@ -46,15 +51,31 @@ export class MasterlistComponent implements OnInit {
     return Array(result);
   }
 
+  sort() {
+    if (this.ascendingSort){
+      this.data.sort(function(a, b){
+         return a.name.localeCompare(b.name)
+      })
+    }
+    else {
+      this.data.sort(function(a, b){
+         return -1*(a.name.localeCompare(b.name))
+      })
+    }
+  }
+
   getCompanies(): void {
     this.service.getCompaniesHTTP().subscribe(
-      result => this.data = result
+      result => {
+        this.data = result
+        this.sort()
+      }
     )
   }
 
-  dropCompany(id: string): void {
+  dropCompany(index: number): void {
     if (confirm("Are you sure?")){
-      this.service.dropCompanyHTTP(id).subscribe(_ => this.getCompanies())
+      this.service.dropCompanyHTTP(this.data[index]["id"]).subscribe(_ => this.getCompanies())
     }
   }
 
